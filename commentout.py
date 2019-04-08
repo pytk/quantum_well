88ee8fa78a1a7928be0c6e7af4b2c411846e7221
N-Yoshida02 <nao.ny00@ezweb.ne.jp>
2019-04-08 11:17:10 +0900

    practice true or false

--- commentout.py
+++ commentout.py
@@ -2,7 +2,13 @@
 """naofumi
 apple
 """
-n=101
+"""n=101
 for i in range (n):
     if i%10==0 and i!=0:
         print(i)
+"""
+n=int(input())
+if n % 3 == 0 and n != 0 :
+        print('True')
+else:
+        print('False')
